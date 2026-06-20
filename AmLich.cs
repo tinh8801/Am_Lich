bdddd38703a30dc1884c9a6829bd9e9d4a58104f
Huy Thang <huythangn@gmail.com>
2026-06-20 17:12:52 +0700

    sua code

--- AmLich.cs
+++ AmLich.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Chưa fix lỗi tính sai
 namespace AmLich
 {
     public static class LunarYearTools
